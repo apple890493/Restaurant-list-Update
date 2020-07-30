59bc2575b92b0e48181ae4e6da486bb5db112968
nonoLin <46785951+apple890493@users.noreply.github.com>
2020-07-31 00:47:03 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,22 +1,13 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
-const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const methOverride = require('method-override')
-const Restaurant = require('./models/info')
+
 const routes = require('./routes')
+require('./config/mongoose')
 
 const app = express()
 const port = 3000
-
-mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true })
-const db = mongoose.connection
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
@@ -31,3 +22,4 @@ app.use(routes)
 app.listen(port, () => {
   console.log(`It's listening on the localhost:${port}`)
 })
+
