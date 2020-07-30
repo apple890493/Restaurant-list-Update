a40a1e80cdbd05adb72cce54285a139b853167ed
nonoLin <46785951+apple890493@users.noreply.github.com>
2020-07-31 00:40:38 +0800

    Update home.js

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,8 +1,6 @@
 //引用Express 與 Express 路由器
 const express = require('express')
 const router = express.Router()
-const multer = require('multer')
-const upload = multer()
 
 const Restaurant = require('../../models/info')
 
@@ -10,18 +8,7 @@ const Restaurant = require('../../models/info')
 router.get('/', (req, res) => {
   Restaurant.find()
     .lean()
-    .sort({ _id: 'asc' })
-    .then(restaurant => res.render('index', { restaurant: restaurant }))
-    .catch(error => console.log(error))
-})
-
-router.get('/sort', (req, res) => {
-  const sortBy = req.query.by
-
-  console.log('sortBy', sortBy)
-  Restaurant.find()
-    .lean()
-    .sort({ sortBy: '-1' })
+    .sort({ _id: '1' })
     .then(restaurant => res.render('index', { restaurant: restaurant }))
     .catch(error => console.log(error))
 })
