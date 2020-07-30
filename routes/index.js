c7eb30ba292ac0d9341d570e08548c59fb2149e3
nonoLin <46785951+apple890493@users.noreply.github.com>
2020-07-31 00:39:21 +0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,11 @@ const express = require('express')
 const router = express.Router()
 const home = require('./modules/home')
 const restaurants = require('./modules/restaurants')
+const sorts = require('./modules/sorts')
 
 router.use('/', home)
 router.use('/restaurants', restaurants)
+router.use('/sorts', sorts)
 
 //匯出路由器
 module.exports = router
