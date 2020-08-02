5f84fe5e5be85a059a28bc395f6d605e5975926c
nonoLin <46785951+apple890493@users.noreply.github.com>
2020-08-02 13:26:52 +0800

    Update infoSeeder.js

--- models/seeds/infoSeeder.js
+++ models/seeds/infoSeeder.js
@@ -4,19 +4,9 @@ const restaurantData = require('../../restaurant.json').results
 const db = require('../../config/mongoose')
 
 db.once('open', () => {
-  restaurantData.forEach(data => {
-    Restaurant.create({
-      name: `${data.name}`,
-      name_en: `${data.name_en}`,
-      category: `${data.category}`,
-      image: `${data.image}`,
-      location: `${data.location}`,
-      phone: `${data.phone}`,
-      google_map: `${data.google_map}`,
-      rating: `${data.rating}`,
-      description: `${data.description}`,
-    })
-  })
-  console.log('done!')
+  console.log('mongodb connected!')
+  for (i = 1; i <= restaurantList.results.length; i++) {
+    Restaurant.create(restaurantList.results[i])
+  }
+  console.log('Restaurant create done')
 })
-
